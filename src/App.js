7021497cdf6dc5b30a09942b0f966d1d51db53fd
sn-dinesh <sapalyakavitha.gmail.com>
2020-08-01 13:38:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/"> React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
