96fbfe3a08988dfd638db2d0ed2b8f987634a34e
sn-dinesh <sapalyakavitha.gmail.com>
2020-08-02 14:11:35 +0530

    cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -46,7 +46,7 @@ export default class Cart extends Component {
                                         cartItems.reduce((a,c) => a + c.price*c.count, 0)
                                     )}
                                 </div>
-                                <button className="button-primary">Proceed</button>             
+                                <button className="button primary">Proceed</button>             
                             </div>
                         </div>
                     )}
